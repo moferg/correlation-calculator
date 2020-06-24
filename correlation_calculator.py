1fbc0576f89720366cf909bbb56505a54d062880
Marshall Ferguson <moferg01@louisville.edu>
2020-06-24 03:47:48 -0400

    rearranged comments

--- correlation_calculator.py
+++ correlation_calculator.py
@@ -1,3 +1,5 @@
+# Correlation Calculator - Marshall Ferguson - 6/2020
+
 # Imports
 
 import math
@@ -5,8 +7,16 @@ import statistics
 
 # Function Definitions
 
+# Mean Formula
+# M = sum of X / N
+# The mean is the sum of the scores divided by number of scores
+
 def mean_of_list(list_values):
 	return sum(list_values) / len(list_values)
+
+# Standard Deviation Formula
+# SD ^ 2 = sum of (X - M) ^ 2 divided by N
+# Variance is Standard Deviation squared, ie Standard Deviation is square root of Variance
 
 def standard_deviation(list_values):
 	deviation_scores = []
@@ -24,12 +34,22 @@ def standard_deviation(list_values):
 	std_deviation = math.sqrt(variance)
 	return std_deviation
 
+# Z Score Formula
+# Z = (X - M) \ SD
+# Z score is equal to the raw score minus the mean, divided by standard deviation
+
 def z_scores_of_list(list_values):
 	z_scores = []
 	for x in list_values:
 		z_score = (x - mean_of_list(list_values)) / standard_deviation(list_values)
 		z_scores.append(z_score)
 	return z_scores
+
+# Correlation Coefficient Formula
+# r = the sum of Z(x) * Z(y) divided by N
+# r = correlation coefficient
+# Z = Z score
+# N = sample size, number of participants
 
 def correlation_of_two_lists(list_values_a, list_values_b):
 	multiplied_z_scores = []
@@ -42,7 +62,9 @@ def correlation_of_two_lists(list_values_a, list_values_b):
 	# 		multiplied_z_score = x * y
 	# 		multiplied_z_scores.append(multiplied_z_score)
 	# sum_of_multiplied_z_scores = math.fsum(multiplied_z_scores)
-	# Leaving the baove code as a reminder that simple is better than complex
+
+	# Leaving the above code as a reminder that simple is better than complex
+
 	for x, y in zip(z_scores_a, z_scores_b):
 		multiplied_z_scores.append(x * y)
 	sum_of_multiplied_z_scores = math.fsum(multiplied_z_scores)
@@ -57,7 +79,7 @@ def correlation_of_two_lists(list_values_a, list_values_b):
 correlation_values_a = []
 correlation_values_b = []
 
-# Ask the user to input values for correlation for each list
+# Ask the user to input values/scores for first list
 
 while True:
 	print("Type 'DONE' when you have entered all values")
@@ -72,10 +94,14 @@ while True:
 		print("The sum of the first list is", sum(correlation_values_a))
 		break
 
+# Print out the mean, standard deviation, and z scores for first list
+
 print("The mean (average) of the first list is", mean_of_list(correlation_values_a))
 print("The Standard Deviation of the first list is", standard_deviation(correlation_values_a))
 print("The Standard Deviation of the first list (using .pstdev) is", statistics.pstdev(correlation_values_a))
 print("The Z-Scores of the first list are", z_scores_of_list(correlation_values_a))
+
+# Ask the user to input values/scores for second list
 
 while True:
 	print("Type 'DONE' when you have entered all values")
@@ -90,27 +116,13 @@ while True:
 		print("The sum of the second list is", sum(correlation_values_b))
 		break
 
+# Print out the mean, standard deviation, and z scores for second list
+
 print("The mean (average) of the second list is", mean_of_list(correlation_values_b))
 print("The Standard Deviation of the second list is", standard_deviation(correlation_values_b))
 print("The Standard Deviation of the second list (using .pstdev) is", statistics.pstdev(correlation_values_b))
 print("The Z-Scores of the second list are", z_scores_of_list(correlation_values_b))
 
+# Print out the correlation coefficient of the two lists
+
 print("The Correlation between the two lists is", correlation_of_two_lists(correlation_values_a, correlation_values_b))
-
-# Mean Formula
-# M = sum of X / N
-# The mean is the sum of the scores divided by number of scores
-
-# Standard Deviation Formula
-# SD ^ 2 = sum of (X - M) ^ 2 divided by N
-# Variance is Standard Deviation squared, ie Standard Deviation is square root of Variance
-
-# Z Score Formula
-# Z = (X - M) \ SD
-# Z score is equal to the raw score minus the mean, divided by standard deviation
-
-# Correlation Coefficient Formula
-# r = the sum of Z(x) * Z(y) divided by N
-# r = correlation coefficient
-# Z = Z score
-# N = sample size, number of participants
